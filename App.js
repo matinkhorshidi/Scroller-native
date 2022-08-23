d085baf4822e7015d2c26bdfdaea4890824640a7
matinkhorshidi <khorshidimatin@gmail.com>
2022-08-23 20:21:06 +0430

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Counter from './src/components/counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Counter></Counter>
+      <Text>Open up App.js to start working on your app! ttt</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+import React, { Fragment, Component } from 'react';
+import { SafeAreaView, StyleSheet, View, Button, Text } from 'react-native';
+
+class Counter extends Component {
+  state = { count: 0 };
+  decrementCount() {
+    let { count } = this.state;
+    count--;
+    this.setState({
+      count,
+    });
+  }
+  incrementCount() {
+    let { count } = this.state;
+    count++;
+    this.setState({
+      count,
+    });
+  }
+  render() {
+    const { count } = this.state;
+    return (
+      <View styles={styles.container}>
+        <Button title="increment" onPress={() => this.incrementCount()} />
+        <Text>{count}</Text>
+        <Button title="decrement" onPress={() => this.decrementCount()} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Counter;
